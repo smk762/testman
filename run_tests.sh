4b7373de0e70139a2fd450aa1f74abab99f1479d
smk762 <smk@komodoplatform.com>
2025-03-21 04:20:05 +0100

    add reown service launch to run_tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-cd config
 if [ ! -e config/coins ] || [ $(find config/coins -mtime -3 | wc -l) -gt 0 ]; then
+    cd config
     wget https://raw.githubusercontent.com/KomodoPlatform/coins/master/coins
+    cp coins ../tcp/coins
+    cp coins ../wasm_rpc/coins
+    cp coins ../newman/coins
+    cd ..
 fi
 
-cp coins ../tcp/coins
-cp coins ../wasm_rpc/coins
-cp coins ../newman/coins
-cd ..
-
 # Start the services
-docker compose up -d kdf-tcp kdf-tcp-hd kdf-wasm kdf-wasm-hd nginx
+docker compose up -d kdf-tcp kdf-tcp-hd kdf-wasm kdf-wasm-hd nginx reown-wc
 
 # Wait for services to be healthy
 echo "Waiting for services to be healthy..."
